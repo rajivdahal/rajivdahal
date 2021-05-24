611d4a6a4df21b1952090511c03ab4af114fb795
unknown <rajivdahal2@gmail.com>
2021-05-24 22:57:28 +0545

    commit first

---
+++ src/Svgskill.js
@@ -0,0 +1,84 @@
+
+import React from 'react'
+import "./Svgskill.css"
+
+export default function SVGSKILL(props) {
+   
+    return (
+        
+        <>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl1" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">70%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">HTML</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl2" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">60%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">CSS</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl3" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">80%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">JAVASCRIPT</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl4" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">60%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">React js</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl5" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">60%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">Django</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl6" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">50%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">Graphics Design</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl7" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">80%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">Python</text>
+        </svg>
+        </div>
+        <div className="cont_inside">
+        <svg height="200" width="200">
+            <circle cx="100" cy="100" r="50" className="crkl"></circle>
+            <circle cx="100" cy="100" r="50" className="jkl8" >
+            </circle>
+            <text x="100" y="100" textAnchor="middle" fill="white" fontSize="30px" fontFamily="Arial" dy=".3em">45%</text>
+            <text x="100" y="175" textAnchor="middle" fill="white" fontSize="15px" fontFamily="Arial" dy=".3em">Machine Learning</text>
+        </svg>
+        </div>
+        </>
+    )
+}
